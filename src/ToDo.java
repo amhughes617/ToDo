9d1d5389d5e3c98a183040b15db7fdb85310afdd
Alex <amhughes@g.cofc.edu>
2016-02-08 11:49:37 -0500

    need to fix an error

--- src/ToDo.java
+++ src/ToDo.java
@@ -1,3 +1,9 @@
+import com.sun.javafx.scene.control.skin.IntegerFieldSkin;
+import com.sun.javafx.scene.control.skin.TabPaneSkin;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by alexanderhughes on 2/8/16.
  */
@@ -5,5 +11,49 @@ public class ToDo {
 
     public static void main(String[] args) {
 
+        ArrayList<ToDoItem> items = new ArrayList<ToDoItem>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Create to-do item");
+            System.out.println("2. Toggle to-do item");
+            System.out.println("3. List to-do items");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+
+                case "1":
+                    System.out.println("Enter your to-do item:");
+                    String text = scanner.nextLine();
+
+                    items.add(new ToDoItem(text, false));
+                    //ToDoItem item = new ToDoItem(text, false);
+                    break;
+
+                case "2":
+                    System.out.print("Enter the number of the item you want to toggle:");
+                    int itemNum = Integer.valueOf(scanner.nextLine());
+                    ToDoItem item = items.get(itemNum - 1);
+                    item.isDone = !item.isDone;
+                    break;
+
+                case "3":
+                    int i = 1;
+                    for (ToDoItem item : items) {
+                        String checkbox = "[ ] ";
+                        if (item.isDone) {
+                            checkbox = "[x] ";
+                        }
+                        System.out.println(checkbox + i + ". " + item.text);
+                        i++;
+                    }
+                    break;
+
+                default:
+                    System.out.println("Invalid Option");
+
+            }
+        }
     }
 }
---
+++ src/ToDoItem.java
@@ -0,0 +1,13 @@
+/**
+ * Created by alexanderhughes on 2/8/16.
+ */
+public class ToDoItem {
+    public String text;
+    public boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
+
